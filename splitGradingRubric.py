f055cf068205716c057edee574e71458aac832ef
Januario Carreiro <jjc70@duke.edu>
2020-02-08 15:24:50 -0500

    Handles exception where document does not exist

--- splitGradingRubric.py
+++ splitGradingRubric.py
@@ -1,13 +1,19 @@
+# author: Januario Carreiro
+# date: 8 Feb 2020
+
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 for i in range(1,8) :
-	filename = "Lab" + str(i)
-	filepath = filename + "/" + filename
+	filename = 'Lab' + str(i)
+	filepath = filename + '/' + filename
 
-	with open(filepath + ".pdf", 'rb') as infile:
-		reader = PdfFileReader(infile)
-		writer = PdfFileWriter()
-		writer.addPage(reader.getPage(-1))
+	try:
+		with open(filepath + '.pdf', 'rb') as infile:
+			reader = PdfFileReader(infile)
+			writer = PdfFileWriter()
+			writer.addPage(reader.getPage(-1))
 
-		with open(filepath + "Rubric.pdf", 'wb') as outfile:
-			writer.write(outfile)
+			with open(filepath + 'Rubric.pdf', 'wb') as outfile:
+				writer.write(outfile)
+	except FileNotFoundError:
+		print(filename + '.pdf does not exist')
